a095965c1a3d531d2ec64faa63c93168e59fa58f
Silviaortmej <158514066+Silviaortmej@users.noreply.github.com>
2024-02-01 21:06:45 +0100

    Add files via upload

---
+++ Q.1 valores aleatorios.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 15 03:24:07 2023
+
+@author: Fer
+"""
+
+import random
+
+def generar_parametro():
+    p1 = np.random.randit(2,3)  
+    return p1
+
+
+p1 = generar_parametro()
+print("El parámetro p1 generado aleatoriamente es:", p1)
+
+p2 = generar_parametro()
+print("El parámetro p2 generado aleatoriamente es:",p2)
+
